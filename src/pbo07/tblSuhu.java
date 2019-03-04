119a43efdf8c4153b274b6b6ec2970275064b090
Aditya Maulana S <37375461+AMaulanaS@users.noreply.github.com>
2019-03-04 09:10:28 +0700

    Add files via upload

---
+++ src/pbo07/tblSuhu.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo07;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author E
+ */
+class tblSuhu {
+
+    static void setModel(DefaultTableModel defaultTableModel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
